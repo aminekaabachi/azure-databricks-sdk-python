11c01f16f0697b9a93508909433027245f3b7b97
Amine Kaabachi <amine.kaabachi@gmail.com>
2020-09-28 16:06:05 +0200

    adding  non-admin cases

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,11 +5,21 @@ import pytest
 
 def test_client_using_azure_ad_user():
     assert isinstance(Client(auth_method=AuthMethod.AZURE_AD_USER,
+                             databricks_instance="ddd", access_token="ddd"), AzureADUserClient)
+
+def test_client_using_azure_ad_user_non_admin_as_admin():
+    assert isinstance(Client(auth_method=AuthMethod.AZURE_AD_USER,
                              databricks_instance="ddd", access_token="ddd", resource_id="hello"), AzureADUserClient)
 
 def test_client_using_azure_ad_service_principal():
     assert isinstance(Client(auth_method=AuthMethod.AZURE_AD_SERVICE_PRINCIPAL,
                              databricks_instance="ddd", access_token="ddd", management_token="ddd", resource_id="ddd"), AzureADServicePrincipalClient)
+
+
+def test_client_using_azure_ad_service_principal_non_admin():
+    assert isinstance(Client(auth_method=AuthMethod.AZURE_AD_SERVICE_PRINCIPAL,
+                             databricks_instance="ddd", access_token="ddd", management_token="ddd"), AzureADServicePrincipalClient)
+
 
 def test_client_using_personal_access_token():
     assert isinstance(Client(databricks_instance="ddd",
