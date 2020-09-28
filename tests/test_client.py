ba22a83ebe773c628ed0af785f108f95c7c56c9c
Amine Kaabachi <amine.kaabachi@gmail.com>
2020-09-28 16:03:31 +0200

    adding new test for sp auth

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,11 +7,13 @@ def test_client_using_azure_ad_user():
     assert isinstance(Client(auth_method=AuthMethod.AZURE_AD_USER,
                              databricks_instance="ddd", access_token="ddd"), AzureADUserClient)
 
+def test_client_using_azure_ad_service_principal():
+    assert isinstance(Client(auth_method=AuthMethod.AZURE_AD_SERVICE_PRINCIPAL,
+                             databricks_instance="ddd", access_token="ddd"), AzureADServicePrincipalClient)
 
 def test_client_using_personal_access_token():
     assert isinstance(Client(databricks_instance="ddd",
                              personal_access_token="ddd"), PersonalAccessTokenClient)
-
 
 def test_wrong_auth_method():
     with pytest.raises(Exception):
