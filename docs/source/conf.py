614ed09a1f829e56a4cda04320c61a769de4a839
Amine Kaabachi <amine.kaabachi@gmail.com>
2020-09-29 22:18:12 +0200

    adding readme to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,29 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import re
+import shutil
+import sys
+
+from recommonmark.transform import AutoStructify
+
+# Work around https://github.com/readthedocs/recommonmark/issues/152
+new_readme = []
+
+with open("../../README.md", "r") as r:
+    lines = r.readlines()
+    for l in lines:
+        nl = re.sub("\[!\[[\w\s]+\]\(", "[![](", l)
+        new_readme.append(nl)
+
+with open("README.md", "w") as n:
+    n.writelines(new_readme)
+
+# apparently index.rst can't search for markdown not in the same directory
+# shutil.copy("../../CONTRIBUTING.md", ".")
+
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -27,10 +50,15 @@ release = '0.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+source_suffix = ['.rst', '.md']
+
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    'sphinx_markdown_tables',
+    'recommonmark',
+    'sphinx.ext.autodoc',
+    #'sphinx.ext.intersphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +81,15 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,7 +10,7 @@ azure-databricks-sdk-python documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
+   README <README.md>
 
 Indices and tables
 ==================
